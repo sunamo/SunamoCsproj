56ee16a3bcbbef8f545320c7ce00ee4de272c48a
sunamo <radek.jancik@sunamo.cz>
2024-01-10 11:47:44 +0100

    .

---
+++ GlobalUsings.cs
@@ -0,0 +1,17 @@
+
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Xml;
+global using System.Xml.Linq;
+
+global using Microsoft.Build.Construction;
+
+global using SunamoCsproj._sunamo.swod;
+global using SunamoCsproj.Data;
+global using SunamoCsproj.Items;
+
+global using SunamoExceptions;
+global using SunamoCsproj;
