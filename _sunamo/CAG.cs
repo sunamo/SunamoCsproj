eeed5243c0263f538ffb8a68e5bbb89f067dcc41
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-02-04 12:09:02 +0100

    .

--- _sunamo/CAG.cs
+++ _sunamo/CAG.cs
@@ -5,32 +5,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SunamoCsproj._sunamo
-{
-    internal class CAG
-    {
-        public static List<T> GetDuplicities<T>(List<T> clipboardL)
-        {
-            List<T> alreadyProcessed;
-            return GetDuplicities<T>(clipboardL, out alreadyProcessed);
-        }
+namespace SunamoCsproj._sunamo;
 
-        internal static List<T> GetDuplicities<T>(List<T> clipboardL, out List<T> alreadyProcessed)
+internal class CAG
+{
+    internal static List<T> GetDuplicities<T>(List<T> clipboardL)
+    {
+        List<T> alreadyProcessed;
+        return GetDuplicities<T>(clipboardL, out alreadyProcessed);
+    }
+
+    internal static List<T> GetDuplicities<T>(List<T> clipboardL, out List<T> alreadyProcessed)
+    {
+        alreadyProcessed = new List<T>(clipboardL.Count);
+        CollectionWithoutDuplicates<T> duplicated = new CollectionWithoutDuplicates<T>();
+        foreach (var item in clipboardL)
         {
-            alreadyProcessed = new List<T>(clipboardL.Count);
-            CollectionWithoutDuplicates<T> duplicated = new CollectionWithoutDuplicates<T>();
-            foreach (var item in clipboardL)
+            if (alreadyProcessed.Contains(item))
             {
-                if (alreadyProcessed.Contains(item))
-                {
-                    duplicated.Add(item);
-                }
-                else
-                {
-                    alreadyProcessed.Add(item);
-                }
+                duplicated.Add(item);
             }
-            return duplicated.c;
+            else
+            {
+                alreadyProcessed.Add(item);
+            }
         }
+        return duplicated.c;
     }
 }
