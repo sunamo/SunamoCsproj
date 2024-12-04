649b1f0adcbe29677e7ab1318672663ef741754a
sunamo.cz <radek.jancik@sunamo.cz>
2024-12-04 17:03:44 +0100

    Removed file

--- _sunamo/CSharpHelper.cs
+++
@@ -1,9 +0,0 @@
-namespace SunamoCsproj._sunamo;
-internal class CSharpHelper
-{
-    internal static string StripComments(string code)
-    {
-        var re = @"(@(?:""[^""]*"")+|""(?:[^""\n\\]+|\\.)*""|'(?:[^'\n\\]+|\\.)*')|//.*|/\*(?s:.*?)\*/";
-        return Regex.Replace(code, re, "$1");
-    }
-}
