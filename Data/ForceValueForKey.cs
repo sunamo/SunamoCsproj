230494d29ae30de3195865b4cbef52abe660edd9
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-03 11:58:46 +0100

    25.1.3.2 Added comment

--- Data/ForceValueForKey.cs
+++ Data/ForceValueForKey.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SunamoCsproj.Data;
+
+/// <summary>
+/// Musí se předávat do každé metody zvlášť, abych věděl přesně, co tam jde!
+/// Předávat přímo do ctor by ušetřilo práci, ale neměl bych přehled co chování té metody ovlivňuje.
+/// </summary>
 public class ForceValueForKey : Dictionary<string, Dictionary<string, string>>
 {
 
