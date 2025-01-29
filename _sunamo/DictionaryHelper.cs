aae5651a68112d9b405297f1589244b4e8155d93
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 22:08:36 +0100

    Remove another unused methods

--- _sunamo/DictionaryHelper.cs
+++ _sunamo/DictionaryHelper.cs
@@ -1,14 +1,4 @@
 namespace SunamoCsproj._sunamo;
 internal class DictionaryHelper
 {
-    internal static List<string> GetListStringFromDictionary(Dictionary<string, string> p)
-    {
-        List<string> vr = [];
-        foreach (var item in p)
-        {
-            vr.Add(item.Key);
-            vr.Add(item.Value);
-        }
-        return vr;
-    }
 }
